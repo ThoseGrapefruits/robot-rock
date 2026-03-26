cc142bf6a9ffbb90c062c98e8a6645212ec659fc
Logan Moore <lmooore@gmail.com>
2026-03-25 22:51:40 -0700

    update leg move functions, mostly working

--- src/state/move.js
+++ src/state/move.js
@@ -1,16 +1,34 @@
 const { scaleAxisToServo } = require('../util/index.js');
 
-const ELBOW_RIGIDITY = 1.5;
-const SHOULDER_RIGIDITY = 1;
+const ELBOW_RIGIDITY = 1.2;
+const SHOULDER_RIGIDITY = 1.4;
 
-const SPEED = 0.5;
-const DEAD_ZONE = 0.02;
+const SPEED = 0.1;
+const DEAD_ZONE = 0.015;
 
 const { PI } = Math
+
+const logNumber = (label, ...ns) => {
+  ns = ns.map(n => `${ n < 0 ? ' ' : '' }${ n.toFixed(2) }`.padStart(7, ' '));
+  console.log(label, ...ns);
+};
 
 const distances = {
   left: 0,
   right: 0
+};
+
+const posElbow = modDist => {
+  if (modDist < 0.5)
+    return 1;
+
+  return 0;
+};
+
+const posShldr = modDist => {
+  if (modDist < 0.5)
+    return -1;
+  return Math.min(1, -1 + modDist);
 };
 
 // Tank-style driving. Left stick controls left legs, right controls right.
@@ -28,36 +46,41 @@ function move(context) {
   const shouldMove = !state.leaned &&
     [ axes.left.magnitude, axes.right.magnitude ]
       .some(magnitude => DEAD_ZONE <= magnitude);
-  
+
   if (shouldMove) {
     const { servos } = state;
 
     [ 'left', 'right' ].forEach((side, sideIndex) => {
       const axis = axes[side];
-      distances[side] += axis.y * SPEED;
+      let otherSide = side === 'left' ? 'right' : 'left';
+      if (!distances[side] && distances[otherSide])
+        distances[side] = distances[otherSide] + PI/2;
+      distances[side] += (axis.y - DEAD_ZONE) * SPEED;
       const distance = distances[side];
       const legs = servos.legs[side] 
+      let modDist;
 
       legs.forEach(({ elbow, shoulder }, legIndex) => {
-        // casting boolean to number, fite me
-        const shift = (legIndex % 2 === sideIndex % 2) * PI / 2;
+        // if (legIndex !== 0) return;
+
+        const timeShift = (legIndex % 2 === sideIndex % 2) * PI / 2;
+        const sideSign = sideIndex ? 1 : -1;
 
         if (Math.abs(axis.y) >= DEAD_ZONE) {
           moved.add(elbow.index);
           moved.add(shoulder.index);
         }
 
-        const elbowPosition = scaleAxisToServo(
-          Math.sin(distance + shift) / ELBOW_RIGIDITY,
+        modDist = Math.abs((distance + timeShift) % PI);
+        logNumber('ds', distance, modDist);
+        elbow.position.goal = scaleAxisToServo(
+          posElbow(modDist) * sideSign / ELBOW_RIGIDITY,
           elbow
         );
-        elbow.position.goal = elbowPosition;
-
-        const shoulderPosition = scaleAxisToServo(
-          Math.sin(-distance + shift) / SHOULDER_RIGIDITY,
+        shoulder.position.goal = scaleAxisToServo(
+          posShldr(modDist) * sideSign / SHOULDER_RIGIDITY,
           shoulder
         );
-        shoulder.position.goal = shoulderPosition;
       });
     })
   } else {
@@ -73,5 +96,16 @@ function move(context) {
     }
   };
 }
+
+function squareWave({ period=2, inset=1, intensity=1 }) {
+  return (distance => ((distance % period) < inset) ? intensity : -intensity);
+};
+
+function triangleWave(period, intensity=1) {
+  let periodHalf = period / 2;
+  return (distance => distance % periodHalf < period
+    ? distance % period
+    : period - distance % period);
+};
 
 module.exports = move;
--- src/util/scale-axis-to-servo.js
+++ src/util/scale-axis-to-servo.js
@@ -3,6 +3,7 @@ function scaleAxisToServo(input, servo) {
 
   let result;
 
+  input = Math.min(1.0, Math.max(-1.0, input));
   if (input > 0) {
     result = input * Math.abs(neutral - max) + neutral;
   } else {
